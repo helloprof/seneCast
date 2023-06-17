cd2304a6f9f4062f2075506f6b98fe6061ab9281
helloprof <suluxan.seneca@gmail.com>
2023-06-17 18:27:22 -0400

    form text data routes

--- server.js
+++ server.js
@@ -5,6 +5,8 @@ const path = require("path")
 
 const HTTP_PORT = process.env.PORT || 8080;
 
+app.use(express.urlencoded({ extended: true }));
+
 function onHttpStart() {
   console.log("server is ready on " + HTTP_PORT + " 🚀🚀🚀!!");
 }
@@ -25,6 +27,19 @@ app.get("/channels", (req, res) => {
     res.json(channels)
   })
 })
+
+app.get("/channels/add", (req, res) => {
+  res.sendFile(path.join(__dirname, "views/addChannels.html"))
+})
+
+app.post("/channels/add", (req, res) => {
+  tubeService.addChannel(req.body).then(() => {
+    res.redirect("/channels")
+  }).catch((err) => {
+    console.log(err)
+  })
+})
+
 
 app.use((req, res) => {
   res.status(404).send("Page Not Found");
