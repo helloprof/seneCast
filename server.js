9c5b6fe331fe0d4a1b3f6670599f7438dd891b9f
helloprof <suluxan.seneca@gmail.com>
2023-06-18 01:21:07 +0200

    added multer, cloudinary, streamifier also configured cloudinary api keys and new routes for multipart form data

--- server.js
+++ server.js
@@ -3,7 +3,21 @@ const app = express();
 const tubeService = require("./tubeService")
 const path = require("path")
 
+const multer = require("multer");
+const cloudinary = require('cloudinary').v2
+const streamifier = require('streamifier')
+
+const upload = multer(); // no { storage: storage } 
+app.use(express.urlencoded({ extended: true }));
+
 const HTTP_PORT = process.env.PORT || 8080;
+
+cloudinary.config({
+  cloud_name: "",
+  api_key: "",
+  api_secret: "",
+  secure: true
+});
 
 app.use(express.urlencoded({ extended: true }));
 
@@ -27,6 +41,57 @@ app.get("/channels", (req, res) => {
     res.json(channels)
   })
 })
+
+app.get("/videos/new", (req, res) => {
+  res.sendFile(path.join(__dirname, "/views/addVideos.html"))
+})
+
+app.post("/videos/new", upload.single("video"), (req, res) => {
+  if (req.file) {
+    let streamUpload = (req) => {
+      return new Promise((resolve, reject) => {
+        let stream = cloudinary.uploader.upload_stream(
+          (error, result) => {
+            if (result) {
+              resolve(result);
+            } else {
+              reject(error);
+            }
+          }
+        );
+
+        streamifier.createReadStream(req.file.buffer).pipe(stream);
+      });
+    };
+
+    async function upload(req) {
+      let result = await streamUpload(req);
+      console.log(result);
+      return result;
+    }
+
+    upload(req).then((uploaded) => {
+      processPost(uploaded.url);
+    });
+  } else {
+    processPost("");
+  }
+
+  function processPost(uploadURL) {
+    req.body.video = uploadURL;
+    tubeService.addVideo(req.body).then(() => {
+      res.redirect("/videos")
+    }).catch((err) => {
+      res.redirect("/videos/add")
+      console.log(err)
+      
+    })
+    // TODO: Process the req.body and add it as a new Blog Post before redirecting to /posts
+  }
+
+
+})
+
 
 app.get("/channels/add", (req, res) => {
   res.sendFile(path.join(__dirname, "views/addChannels.html"))
