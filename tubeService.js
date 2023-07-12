a71ee44a1444a66285fb20a9bb7a909d4df34881
helloprof <suluxan.seneca@gmail.com>
2023-07-12 13:22:21 -0400

    initiate sequelize object with credentials and add new schemas/models; refactored functions to use the database for channels

--- tubeService.js
+++ tubeService.js
@@ -1,22 +1,61 @@
 const fs = require("fs")
-let videos = []
-let channels = []
+let videos = [0]
+let channels = [0]
+
+const env = require("dotenv")
+env.config()
+
+const Sequelize = require("sequelize")
+
+var sequelize = new Sequelize(process.env.POSTGRES_DB, process.env.POSTGRES_DB, process.env.POSTGRES_PASSWORD, {
+  host: process.env.POSTGRES_HOST,
+  dialect: 'postgres',
+  port: 5432,
+  dialectOptions: {
+      ssl: { rejectUnauthorized: false }
+  },
+  query: { raw: true }
+});
+
+// sequelize.authenticate().then(() => {
+//   console.log("credentials are good!")
+// })
+
+var Channel = sequelize.define('Channel', {
+  channelID: {
+    type: Sequelize.INTEGER,
+    primaryKey: true,
+    autoIncrement: true
+  },
+  channel: Sequelize.STRING,
+  // displayPic: 
+})
+
+var Video = sequelize.define('Video', {
+  videoID: { 
+    type: Sequelize.INTEGER,
+    primaryKey: true,
+    autoIncrement: true
+  },
+  title: Sequelize.STRING,
+  description: Sequelize.STRING,
+  video: Sequelize.STRING,
+  date: Sequelize.DATE,
+  comments: Sequelize.ARRAY(Sequelize.TEXT),
+  likes: Sequelize.INTEGER,
+  views: Sequelize.INTEGER
+})
+
+Video.belongsTo(Channel, {foreignKey: 'channelID'})
 
 module.exports.initialize = () => {
   return new Promise((resolve, reject) => {
-    fs.readFile("./data/videos.json", "utf-8", (err, data) => {
-      if (err) {
-        reject(`ERR: ${err}`)
-      }
-      videos = JSON.parse(data)
-
-      fs.readFile("./data/channels.json", "utf-8", (err, data) => {
-        if (err) {
-          reject(`ERR: ${err}`)
-        }
-        channels = JSON.parse(data)
-        resolve("success")
-      })
+    sequelize.sync().then(() => {
+      console.log("POSTGRES DB SYNC'D!")
+      resolve()
+    }).catch((err) => {
+      console.log("POST GRES DB COULD NOT BE SYNC'D, ERR: "+err)
+      reject()
     })
   })
 }
@@ -34,10 +73,16 @@ module.exports.getAllVideos = () => {
 
 module.exports.getAllChannels = () => {
   return new Promise((resolve, reject) => {
-    if (channels.length == 0) {
-      reject("no videos found!")
-    }
-    resolve(channels)
+    // if (channels.length == 0) {
+    //   reject("no videos found!")
+    // }
+    // resolve(channels)
+    Channel.findAll().then((channels) => {
+      console.log(channels)
+      resolve(channels)
+    }).catch((err) => {
+      console.log("CHANNELS ERROR: "+err)
+    })
   })
 }
 
@@ -55,12 +100,23 @@ module.exports.addVideo = (video) => {
 }
 module.exports.addChannel = (channel) => {
   return new Promise((resolve, reject) => {
+    // if (channel) {
+    //   channel.id = channels.length + 1
+    //   channels.push(channel)
+    //   resolve("success")
+    // } else {
+    //   reject("no channel data available")
+    // }
+
     if (channel) {
-      channel.id = channels.length + 1
-      channels.push(channel)
-      resolve("success")
+      Channel.create(channel).then(() => {
+        console.log("CHANNEL CREATED!")
+        resolve()
+      }).catch((err) => {
+        console.log("CHANNEL CREATION ERROR: "+err)
+      })
     } else {
-      reject("no channel data available")
+      reject()
     }
   })
 }
